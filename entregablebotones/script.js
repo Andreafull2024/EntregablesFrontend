f4e365318b4cb4ca1068e765c85e6fb04034a932
Andreafullstack <andreasanjuan21@gmail.com>
2024-07-14 19:30:41 -0300

    botones divs

---
+++ entregablebotones/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let button1 = document.querySelector('.button1');
+let button2 = document.querySelector('.button2');
+let box1 = document.getElementById('div1');
+let box2 = document.getElementById('div2');
+
+button1.addEventListener('click', function() {
+    box1.style.visibility = 'hidden';
+    box2.style.visibility = 'visible';
+});
+
+button2.addEventListener('click', function() {
+    box1.style.visibility = 'visible';
+    box2.style.visibility = 'hidden';
+});
