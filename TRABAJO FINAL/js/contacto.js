39853510172ef241a854d1a48e35294d02ef48a5
Andreafullstack <andreasanjuan21@gmail.com>
2024-07-31 15:12:36 -0300

    trabajofinal

--- TRABAJO FINAL/js/contacto.js
+++ TRABAJO FINAL/js/contacto.js
@@ -1,27 +1,29 @@
 "use strict";
-let nombre = document.getElementById("nombre");
-let apellido = document.getElementById("apellido");
-let correo = document.getElementById("correo");
-let telefono = document.getElementById("telefono");
-let mensaje = document.getElementById("mensaje");
-let btnEnviar = document.getElementById("enviar"); 
 
-let informacion = [];
+let btnEnviar = document.getElementById("enviar");
 
 btnEnviar.addEventListener("click", (e) => {
-    e.preventDefault(); // Previene la acción del form de actualizar la página
-    
-    informacion [0] = nombre.value;
-    informacion [1] = apellido.value;
-    informacion [2] = correo.value;
-    informacion [3] = telefono.value;
-    informacion [4] = mensaje.value;
+    e.preventDefault(); // Prevent the form from submitting
+
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let correo = document.getElementById("correo").value;
+    let telefono = document.getElementById("telefono").value;
+    let mensaje = document.getElementById("mensaje").value;
+
+    let informacion = [];
+
+    informacion[0] = "Nombre: " + nombre;
+    informacion[1] = "Apellido: " + apellido;
+    informacion[2] = "Correo: " + correo;
+    informacion[3] = "Teléfono: " + telefono;
+    informacion[4] = "Mensaje: " + mensaje;
 
     let contenidoFormateado = informacion.join("\n");
 
-//Crear el Blob con el contenido formateado
+    // Create a Blob with the formatted content
     let blob = new Blob([contenidoFormateado], { type: "text/plain;charset=utf-8" });
 
-// Guarda la información en un archivo .txt// libreria
-    saveAs(blob, "contact.txt"); 
+    // Save the information to a .txt file
+    saveAs(blob, "contact.txt");
 });
