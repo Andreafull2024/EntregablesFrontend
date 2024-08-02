64d0d04a18910938e373b3c19fb6698860e24030
Andreafullstack <andreasanjuan21@gmail.com>
2024-08-02 18:34:13 -0300

    trabajo final

--- TRABAJO FINAL/js/contacto.js
+++ TRABAJO FINAL/js/contacto.js
@@ -1,16 +1,16 @@
 "use strict";
 
-let btnEnviar = document.getElementById("enviar");
+let btnEnviar = document.getElementById("enviar");/*se selecciona el boton  con el id enviar  y se almacena en esta variable */
 
-btnEnviar.addEventListener("click", (e) => {
-    e.preventDefault(); // Prevent the form from submitting
+btnEnviar.addEventListener("click", (e) => {/* se añade un evento  al botón enviar, se hace clik y se ejecuta la función*/
+    e.preventDefault(); /*esto hace que podamos manejar el envio del form*/
 
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let correo = document.getElementById("correo").value;
     let telefono = document.getElementById("telefono").value;
     let mensaje = document.getElementById("mensaje").value;
-
+/*obetenemos los valores ingresados por el usuario*/
     let informacion = [];
 
     informacion[0] = "Nombre: " + nombre;
@@ -18,12 +18,12 @@ btnEnviar.addEventListener("click", (e) => {
     informacion[2] = "Correo: " + correo;
     informacion[3] = "Teléfono: " + telefono;
     informacion[4] = "Mensaje: " + mensaje;
-
+/* se inicia el array vacio de info, luego se llenan las posicicones con cadenas de texto que tienen los datos del form*/
     let contenidoFormateado = informacion.join("\n");
+/*sirve para hacer un salto de linea*/
+    
+    let blob = new Blob([contenidoFormateado], { type: "text/plain;charset=utf-8" });/* se crea este objeto que representa un archivo de datos.*/
 
-    // Create a Blob with the formatted content
-    let blob = new Blob([contenidoFormateado], { type: "text/plain;charset=utf-8" });
-
-    // Save the information to a .txt file
+     /*se guarda la info como texto*/
     saveAs(blob, "contact.txt");
 });
